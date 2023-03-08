91fe71ae3095465577ec7df1864593ac2ef39da2
_Pier__ <79757325+Pier-cmd@users.noreply.github.com>
2023-03-08 16:25:33 +0100

    Add files via upload
    Inserire 10 citta

--- connectionDB.java
+++ connectionDB.java
@@ -3,36 +3,56 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.*;
 
+
 public class connectionDB {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-Connection conn = null;
+        Connection conn = null;
 
-try {
-conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "root");
-if (conn != null) {
-System.out.println("Connesso");
-} else {
-System.out.println("Connessione fallita");
-}
-// Prova lettura db
-String query = "SELECT * FROM city WHERE name like 'Bari'";
-Statement stm = conn.createStatement();
-ResultSet rs = stm.executeQuery(query);
-while (rs.next()) {
-String tableFormat = String.format("ID: %d Name: %s CountryCode: %s District: %s Population: %d",
-rs.getInt(1),
-rs.getString(2),
-rs.getString(3),
-rs.getString(4),
-rs.getInt(5));
-System.out.println(tableFormat);
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "root");
+            if (conn != null) {
+                System.out.println("Connesso");
+            } else {
+                System.out.println("Connessione fallita");
+            }
+            
+            String[][] cities = {
+                {"Carovigno", "ITA", "Apulia", "16770"},
+                {"Ostuni", "ITA", "Apulia", "30000"},
+                {"Brindisi", "ITA", "Apulia", "60000"},
+                {"San Vito dei Normanni", "ITA", "Apulia", "15000"},
+                {"Ceglie Messapica", "ITA", "Apulia", "16000"},
+                {"Martina Franca", "ITA", "Apulia", "50252"},
+                {"Cisternino", "ITA", "Apulia", "12000"},
+                {"Casalini", "ITA", "Apulia", "15"},
+                {"San Michele Salentino", "ITA", "Apulia", "14252"},
+                {"Mesagne", "ITA", "Apulia", "22000"}
+        };
+        
+        for (String[] city : cities) {
+            String insertQuery = "INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(insertQuery);
+            pstmt.setString(1, city[0]);
+            pstmt.setString(2, city[1]);
+            pstmt.setString(3, city[2]);
+            pstmt.setInt(4, Integer.parseInt(city[3]));
+            pstmt.executeUpdate();
+        }
+            
+            // Prova lettura db
+            String query = "SELECT * FROM world.city WHERE district = 'Apulia'";
 
-}
-} catch (Exception e) {
-e.printStackTrace();
-}
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+            while (rs.next()) {
+                String cityName = rs.getString("Name");
+                System.out.println(cityName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-}
+    }
 
 }
